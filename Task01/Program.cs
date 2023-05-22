4ed0419d2c274ab38d34b4abf65815e83c6d8174
taydrek <taydrek@mail.ru>
2023-05-23 01:55:56 +0300

    added Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter a number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num == num1 * num1)
+{
+    Console.WriteLine($"Да, {num} является квадратом числа {num}");
+}
+else Console.WriteLine($"Нет, {num} не является квадратом числа {num}");
+
