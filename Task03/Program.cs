8189c41b165d444fa5f830e3b6437858fdcbe297
taydrek <taydrek@mail.ru>
2023-05-23 02:00:53 +0300

    added Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Введите число, соответствующее дню недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 1 && num <= 7)
+{
+    if (num == 1)
+    {   
+    Console.WriteLine("Понедельник");
+    }
+    else if (num == 2)
+    {
+    Console.WriteLine("Вторник");
+    }
+    else if (num == 3)
+    {   
+    Console.WriteLine("Среда");
+    }
+    else if (num == 4)
+    {
+    Console.WriteLine("Четверг");
+    }
+    else if (num == 5)
+    {   
+    Console.WriteLine("Пятница");
+    }
+    else if (num == 6)
+    {
+    Console.WriteLine("Субота");
+    }
+    else if (num == 7)
+    {   
+    Console.WriteLine("Воскресенье");
+    }
+}
+else
+{
+    Console.WriteLine("Введено неправильное число.");
+}    
+    
+    
