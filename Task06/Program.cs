49789a5ac215bbb5dc2df0d7310a60ff4b260bdd
taydrek <taydrek@mail.ru>
2023-05-25 17:34:05 +0300

    fix Task02

---
+++ Task06/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, 
+// которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите  чосло ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number / 2)
+{
+    Console.WriteLine($"{number} Является четным ");
+}
+else
+    Console.WriteLine($"{number} не четное");
+
