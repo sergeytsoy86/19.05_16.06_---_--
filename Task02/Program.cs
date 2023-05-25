2ce829d6454b236f291b316cb0315a18b2fabc4f
taydrek <taydrek@mail.ru>
2023-05-25 17:37:45 +0300

    changes Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -3,7 +3,7 @@
 // какое число большее, а какое меньшее.
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -
+// a = -9 b = -3 -> max = -3
 
 Console.Write("Введите первое число:");
 int num1 = Convert.ToInt32(Console.ReadLine());
