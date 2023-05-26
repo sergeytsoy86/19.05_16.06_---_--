756b50493f9c1e8206afff22201554d1c3b41f6d
taydrek <taydrek@mail.ru>
2023-05-26 22:29:57 +0300

    added Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 13: Напишите программу, 
+// которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int ThirdDigit(int number)
+{
+    int result = -1;
+    if (number >= 100)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        result = number % 10;
+    }
+    return result;
+}
+
+
+if (ThirdDigit(number1) == -1)
+    Console.WriteLine("третьей цифры нет");
+else
+    Console.WriteLine($"Третья цифра: {ThirdDigit(number1)}");
