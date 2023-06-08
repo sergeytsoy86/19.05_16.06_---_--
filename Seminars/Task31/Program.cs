681c8a67b96025fb4e07171c5056803e83c7bf4a
taydrek <taydrek@mail.ru>
2023-06-08 15:47:08 +0300

    added Task34

--- Seminars/Task31/Program.cs
+++ Seminars/Task31/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Задача 31: Задайте массив из 12 элементов, заполненный
 // случайными числами из промежутка [-9, 9]. 
-
-//Найдите сумму отрицательных и положительных элементов массива.
-// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма
+// Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3, 9, -8, 1, 0, -7, 2, -1, 8, -3, -1, 6] сумма
 // положительных чисел равна 29, сумма отрицательных равна
 // -20.
+
 
 int[] GetSumPositiveNegativElem(int[] arr)
 {
@@ -43,14 +43,25 @@ int GetSumNegativeElem(int[] arr)
     
     return sum;
 }
-int[] sumPositiveNegativElem = GetSumPositiveNegativElem(array);
-Console.WriteLine($"Сумма положительных элементов = {sumPositiveNegativElem[0]}");
-Console.WriteLine($"Сумма отрицательных элементов = {sumPositiveNegativElem[1]}");
+// int[] sumPositiveNegativElem = GetSumPositiveNegativElem(array);
+// Console.WriteLine($"Сумма положительных элементов = {sumPositiveNegativElem[0]}");
+// Console.WriteLine($"Сумма отрицательных элементов = {sumPositiveNegativElem[1]}");
 
-int sumPositiveElem = GetSumPositiveElem(array);
-int sumNegativeElem = GetSumNegativeElem(array);
-Console.WriteLine($"Сумма положительных элементов = {sumPositiveElem}");
-Console.WriteLine($"Сумма отрицательных элементов = {sumNegativeElem}");
+// int sumPositiveElem = GetSumPositiveElem(array);
+// int sumNegativeElem = GetSumNegativeElem(array);
+// Console.WriteLine($"Сумма положительных элементов = {sumPositiveElem}");
+// Console.WriteLine($"Сумма отрицательных элементов = {sumNegativeElem}");
+
+int[] CreateArrayRndint(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max +1);
+    }
+    return arr;
+}
 
 //нахождение среднего числа 
 double[] CreateArrayRndDouble(int size, int min, int max, int round = 2)
@@ -66,7 +77,7 @@ double[] CreateArrayRndDouble(int size, int min, int max, int round = 2)
     return arr;
 }
 
-void PrintArrayDouble(double[] arr, string sep = ",")
+void PrintArray(double[] arr, string sep = ",")
 {
     for (int i = 0; i < arr.Length; i++)
     {
@@ -74,3 +85,8 @@ void PrintArrayDouble(double[] arr, string sep = ",")
         else Console.Write ($"{arr[i]}");
     }
 }
+
+// int[] array = CreateArrayRndint(12, -9, 9);
+// Console.Write("[");
+// PrintArray(array);
+// Console.WriteLine("]");
