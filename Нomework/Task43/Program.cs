2bead602240c3eb5df6f8df4b606dda6bd0d2fde
taydrek <taydrek@mail.ru>
2023-06-13 14:52:13 +0300

    added Task43

---
+++ Нomework/Task43/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 43: Напишите программу, 
+// которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double GetNumber(string prompt)
+{
+    Console.Write($"Enter {prompt} value: ");
+    double number = Convert.ToDouble(Console.ReadLine());
+    return number;
+}
+
+void GetIntersectionPoint(double k1, double b1, double k2, double b2)
+{
+    if (k1 == k2)
+    {
+        if (b1 == b2)
+        {
+            Console.WriteLine("The lines are identical");
+            return;
+        }
+        
+        Console.WriteLine("The lines are parallel");
+        return;
+    }
+
+    double x = Math.Round((b2 - b1) / (k1 - k2), 3); // formula for x is (b2 - b1) / (k1 - k2)
+    double y = Math.Round(k1 * x + b1, 3); // formula for y is k1 * x + b1
+    Console.WriteLine($"The intersection point is ({x}, {y})");
+}
+
+double k1 = GetNumber("k1");
+double b1 = GetNumber("b1");
+double k2 = GetNumber("k2");
+double b2 = GetNumber("b2");
+
+GetIntersectionPoint(k1, b1, k2, b2);
