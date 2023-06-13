f6da9c278f734637c8d10d6fae7d0c943f541377
taydrek <taydrek@mail.ru>
2023-06-13 16:11:34 +0300

    added Task47

--- Seminars/Task39/Program.cs
+++ Seminars/Task39/Program.cs
@@ -4,3 +4,44 @@
 // [1 2 3 4 5] -> [5 4 3 2 1]
 // [6 7 3 6] -> [6 3 7 6]
 
+
+int[] CreateFillArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    int length = arr.Length;
+    Console.Write("[");
+    for (int i = 0; i < length; i++)
+    {
+        if (length - i > 1)
+            Console.Write($"{arr[i]}, ");
+        else
+            Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void ReverseArray(int[] arr)
+{
+    int len = arr.Length;
+    for (int i = 0; i < len / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[len - 1 - i];
+        arr[len - 1 - i] = temp;
+    }
+}
+
+int[] array = CreateFillArray(5, 1, 10);
+PrintArray(array);
+ReverseArray(array);
+PrintArray(array);
---
+++ Нomework/Task47/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47. Задайте двумерный массив размером m × n, 
+// заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(-99, 99) / 10.0;
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6}");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] array2d = CreateMatrixRndInt(3, 4, 3, 4);
+PrintMatrix(array2d);
